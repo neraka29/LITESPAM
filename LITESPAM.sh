904e511e9eaa8e1af897442fe3d0d79b9e68dbc6
neraka29 <43645033+neraka29@users.noreply.github.com>
2018-10-05 19:01:37 +0700

    LITESPAM
    neraka29 / LITESPAM
    Kode  Masalah 1 Tarik permintaan 0 Proyek 0 Pulsa Wiki     
    LITESPAM.sh
    
     menguasai Terakhir diubah oleh neraka29 1 jam yang lalu
    #! / system / xbin / bash
    bersih
    toilet -f slant --gay " LiteSpam "
    tidur 1
    echo  " \ 033 [31; 1m Macam-macam alat untuk spam "
    tidur 1
    echo  " \ 033 [33; 1m Penulis: Mr.MAH "
    tidur 1
    echo  " \ 033 [32; 1m ★ contack: +6283823938886 "
    tidur 1
    echo  " \ 033 [34; 1m ★ Indonesia FREEDOM security "
    tidur 1
    echo  " \ 033 [36; 1m Spam Yang Tersedia: "
    echo  " 1. Tokopedia "
    echo  " 2. Telkomsel "
    echo  " 3. Matahari Mall "
    echo  " 4. PHD "
    echo  " 5. Jd.Id "
    echo  " 6. Email Bomber "
    echo  " 0. Keluar "
    echo  " \ 033 [30; 1m81. Informasi Alat Ini "
    echo  " \ 033 [33; 1m Pilih Angka: "
    baca mrrm
    jika [ $ mrMAH  = 1] || [ $ mrMAH  = 1]
    kemudian
    bersih
    echo  " \ 033 [34; 1m "
    figlet " tokopedia "
    php 1.php
    fi
    
    jika
    [ $ mrrm  = 2] || [ $ mrrm  = 2]
    kemudian
    bersih
    echo  " \ 033 [31; 1m "
    toilet " T-Sel "
    php 2.php
    fi
    
    jika [ $ mrrm  = 3] || [ $ mrrm  = 3]
    kemudian
    bersih
    echo  " \ 033 [31; 1m "
    figlet " Mthr Mall "
    php 3.php
    fi
    
    
    jika [ $ mrrm  = 4] || [ $ mrrm  = 4]
    kemudian
    bersih
    toilet -f mono12 -F gay " PHD "
    echo  " \ 033 [30; 1m "
    php 4.php
    fi
    
    jika
    [ $ mrrm  = 5] || [ $ mrrm  = 5]
    kemudian
    bersih
    toilet -f mono12 -F gay " Jd.Id "
    echo  " \ 033 [33; 1m "
    php 5.php
    fi
    
    jika
    [ $ mrrm  = 6] || [ $ mrrm  = 6]
    kemudian
    bersih
    toilet -f standar -F gay " Email Bomber "
    echo  " \ 033 [36; 1m "
    python2 6.py
    fi
    
    jika
    [ $ mrrm  = 81] || [ $ mrrm  = 81]
    kemudian
    bersih
    toilet -f slant --gay " LiteSpam "
    echo  " \ 033 [31; Alat 1mNama: LiteSpam "
    tidur 1
    echo  " \ 033 [33; 1mKarya: Mr.MAH (V2 Oleh Mr.JARWO) "
    tidur 1
    echo  " \ 033 [32; 1mVersi: v2 "
    tidur 1
    echo  " \ 033 [36; 1mTeam: ★ Indonesia FREEDOM Security ★ "
    tidur 1
    echo  " \ 033 [34; 1mInformasi Lebih Lanjut Kunjungi: https://youtube.com/IndonesiaFRERDOMSecurity "
    echo  " \ 033 [31; 1m Spesial Terima Kasih Kepada: "
    echo  " \ 033 [36; 1m "
    echo  " Handika Pratama "
    echo  "J A R W O"
    gema  " INDONESIA Cyber ​​Team "
    echo  " SBG-Team "
    echo  " Tim BLANAKAN Defacer "
    tidur 1
    echo  " Dan All Member Indonesia Security  "
    echo  " \ 033 [30; 1m tunggu 8 detik "
    tidur 8
    sh LITESPAM.sh
    fi
    
    jika
    [ $ mrrm  = 0] || [ $ mrrm  = 0]
    kemudian
    echo  " \ 033 [31; 1m Keluar "
    tidur 1
    echo  " \ 033 [32; 1m Sampai berjumpa lagi :) "
    tidur 1
    fi
    
     Versi desktop Keluar

---
+++ LITESPAM.sh
@@ -0,0 +1,120 @@
+
+
+neraka29 / LITESPAM
+Kode  Masalah 1 Tarik permintaan 0 Proyek 0 Pulsa Wiki     
+LITESPAM.sh
+
+ menguasai Terakhir diubah oleh neraka29 1 jam yang lalu
+#! / system / xbin / bash
+bersih
+toilet -f slant --gay " LiteSpam "
+tidur 1
+echo  " \ 033 [31; 1m Macam-macam alat untuk spam "
+tidur 1
+echo  " \ 033 [33; 1m Penulis: Mr.MAH "
+tidur 1
+echo  " \ 033 [32; 1m ★ contack: +6283823938886 "
+tidur 1
+echo  " \ 033 [34; 1m ★ Indonesia FREEDOM security "
+tidur 1
+echo  " \ 033 [36; 1m Spam Yang Tersedia: "
+echo  " 1. Tokopedia "
+echo  " 2. Telkomsel "
+echo  " 3. Matahari Mall "
+echo  " 4. PHD "
+echo  " 5. Jd.Id "
+echo  " 6. Email Bomber "
+echo  " 0. Keluar "
+echo  " \ 033 [30; 1m81. Informasi Alat Ini "
+echo  " \ 033 [33; 1m Pilih Angka: "
+baca mrrm
+jika [ $ mrMAH  = 1] || [ $ mrMAH  = 1]
+kemudian
+bersih
+echo  " \ 033 [34; 1m "
+figlet " tokopedia "
+php 1.php
+fi
+
+jika
+[ $ mrrm  = 2] || [ $ mrrm  = 2]
+kemudian
+bersih
+echo  " \ 033 [31; 1m "
+toilet " T-Sel "
+php 2.php
+fi
+
+jika [ $ mrrm  = 3] || [ $ mrrm  = 3]
+kemudian
+bersih
+echo  " \ 033 [31; 1m "
+figlet " Mthr Mall "
+php 3.php
+fi
+
+
+jika [ $ mrrm  = 4] || [ $ mrrm  = 4]
+kemudian
+bersih
+toilet -f mono12 -F gay " PHD "
+echo  " \ 033 [30; 1m "
+php 4.php
+fi
+
+jika
+[ $ mrrm  = 5] || [ $ mrrm  = 5]
+kemudian
+bersih
+toilet -f mono12 -F gay " Jd.Id "
+echo  " \ 033 [33; 1m "
+php 5.php
+fi
+
+jika
+[ $ mrrm  = 6] || [ $ mrrm  = 6]
+kemudian
+bersih
+toilet -f standar -F gay " Email Bomber "
+echo  " \ 033 [36; 1m "
+python2 6.py
+fi
+
+jika
+[ $ mrrm  = 81] || [ $ mrrm  = 81]
+kemudian
+bersih
+toilet -f slant --gay " LiteSpam "
+echo  " \ 033 [31; Alat 1mNama: LiteSpam "
+tidur 1
+echo  " \ 033 [33; 1mKarya: Mr.MAH (V2 Oleh Mr.JARWO) "
+tidur 1
+echo  " \ 033 [32; 1mVersi: v2 "
+tidur 1
+echo  " \ 033 [36; 1mTeam: ★ Indonesia FREEDOM Security ★ "
+tidur 1
+echo  " \ 033 [34; 1mInformasi Lebih Lanjut Kunjungi: https://youtube.com/IndonesiaFRERDOMSecurity "
+echo  " \ 033 [31; 1m Spesial Terima Kasih Kepada: "
+echo  " \ 033 [36; 1m "
+echo  " Handika Pratama "
+echo  "J A R W O"
+gema  " INDONESIA Cyber ​​Team "
+echo  " SBG-Team "
+echo  " Tim BLANAKAN Defacer "
+tidur 1
+echo  " Dan All Member Indonesia Security  "
+echo  " \ 033 [30; 1m tunggu 8 detik "
+tidur 8
+sh LITESPAM.sh
+fi
+
+jika
+[ $ mrrm  = 0] || [ $ mrrm  = 0]
+kemudian
+echo  " \ 033 [31; 1m Keluar "
+tidur 1
+echo  " \ 033 [32; 1m Sampai berjumpa lagi :) "
+tidur 1
+fi
+
+ Versi desktop Keluar
